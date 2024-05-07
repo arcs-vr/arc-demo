4969bb459dbdd8629591e94590c3a13989d7ad7a
barthy <post@barthy.koeln>
2024-05-07 22:21:14 +0200

    chore: update

--- src/arc-config.js
+++ src/arc-config.js
@@ -1,8 +1,8 @@
 export const config = {
   app: 'mqtt/arcs-vr-demo',
   protocol: 'wss',
-  host: 'mqtt.barthy.koeln',
-  port: 9001,
+  host: '6f8df38f09764d318d9006d18c411267.s1.eu.hivemq.cloud',
+  port: 8884,
   routeApp: 'index',
   routeRemote: 'remote-selector',
   path: ''
